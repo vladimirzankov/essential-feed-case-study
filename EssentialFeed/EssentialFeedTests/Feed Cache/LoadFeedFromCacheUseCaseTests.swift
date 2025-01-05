24a459fd5b9972bc5ca24d296cbd38832298ad4c
Vladimir Zankov <vladimirzankov@outlook.com>
2025-01-05 13:40:56 +0300

    Load command deletes cache on retrieval error

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -74,6 +74,16 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    
+    func test_load_deletesCacheOnRetievalError() {
+        let (sut, store) = makeSUT()
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCacheFeed])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (LocalFeedLoader, FeedStoreSpy) {

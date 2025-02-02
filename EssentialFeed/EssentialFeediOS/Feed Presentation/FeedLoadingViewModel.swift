4710b774bfca39e1ba12a9d40626f0cdbb52d871
Vladimir Zankov <vladimirzankov@outlook.com>
2025-02-02 21:25:24 +0300

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  LoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Занков Владимир Владимирович on 02.02.2025.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Занков Владимир Владимирович on 02.02.2025.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}

28d45e812f6bc666a3522b6a568ae69eeffa41f3
Vladimir Zankov <vladimirzankov@outlook.com>
2025-02-06 21:50:39 +0300

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Занков Владимир Владимирович on 06.02.2025.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}

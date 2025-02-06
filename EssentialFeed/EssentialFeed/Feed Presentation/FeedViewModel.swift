05a375234677f2e7256f0496cf0ca223ac1f3ad7
Vladimir Zankov <vladimirzankov@outlook.com>
2025-02-06 21:49:22 +0300

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Занков Владимир Владимирович on 06.02.2025.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}

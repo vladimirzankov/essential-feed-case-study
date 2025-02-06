fb2b17c44862ab4507c83969c23aa9d1619a3e2e
Vladimir Zankov <vladimirzankov@outlook.com>
2025-02-06 19:45:14 +0300

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Занков Владимир Владимирович on 06.02.2025.
+//
+
+struct FeedErrorViewModel {
+    let message: String?
+}

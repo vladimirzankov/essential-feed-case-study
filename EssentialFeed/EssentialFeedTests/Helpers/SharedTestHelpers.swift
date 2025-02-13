6935a19ff4b3c6e6838b5b3cd2a8da4ccd92a956
Vladimir Zankov <vladimirzankov@outlook.com>
2025-02-13 16:17:47 +0300

    Extract duplicate `anyData` helper into a shared scope

--- EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -7,10 +7,14 @@
 
 import Foundation
 
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
 func anyURL() -> URL {
     return URL(string: "http://any-url.com")!
 }
 
-func anyNSError() -> NSError {
-    return NSError(domain: "any error", code: 0)
+func anyData() -> Data {
+    return Data("any data".utf8)
 }

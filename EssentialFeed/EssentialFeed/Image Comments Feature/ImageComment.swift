2bea7540fbb2825b956d0ca6f8475a163a88b9c5
Vladimir Zankov <vladimirzankov@outlook.com>
2025-02-25 15:49:13 +0300

    Add ImageComment data model

---
+++ EssentialFeed/EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Занков Владимир Владимирович on 25.02.2025.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
